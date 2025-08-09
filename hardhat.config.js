b615c065cb85e3afd485cd4f4258b1d17ece2278
Mobin Saeidi <saeidimobin7@gmail.com>
2025-08-09 16:13:38 +0330

    update2

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || "";
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
+  networks: {
+    sepolia: {
+      url: SEPOLIA_RPC_URL,
+      accounts: PRIVATE_KEY ? [PRIVATE_KEY] : [],
+    },
+  },
 };
