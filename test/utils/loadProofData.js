a2d12c9afc0af53e32e479aef397a6a246060331
Mobin Saeidi <saeidimobin7@gmail.com>
2025-08-13 16:13:34 +0330

    updateloadproofdata

--- test/utils/loadProofData.js
+++ test/utils/loadProofData.js
@@ -5,19 +5,21 @@ function getCalldata(proofPath, publicSignalsPath) {
     const proof = JSON.parse(fs.readFileSync(proofPath, 'utf8'));
     const publicSignals = JSON.parse(fs.readFileSync(publicSignalsPath, 'utf8'));
 
+    
+    const a = [BigInt(proof.pi_a[0]), BigInt(proof.pi_a[1])];
+
    
-    const a = proof.pi_a || proof.a;  
-    const b = proof.pi_b || proof.b; 
-    const c = proof.pi_c || proof.c;  
-    const input = publicSignals;
+    const b = [
+        [BigInt(proof.pi_b[0][1]), BigInt(proof.pi_b[0][0])],
+        [BigInt(proof.pi_b[1][1]), BigInt(proof.pi_b[1][0])]
+    ];
+
+    const c = [BigInt(proof.pi_c[0]), BigInt(proof.pi_c[1])];
 
     
-    return [
-        [BigInt(a[0]), BigInt(a[1])],
-        [[BigInt(b[0][0]), BigInt(b[0][1])], [BigInt(b[1][0]), BigInt(b[1][1])]],
-        [BigInt(c[0]), BigInt(c[1])],
-        input.map(x => BigInt(x))
-    ];
+    const input = publicSignals.map(x => BigInt(x));
+
+    return [a, b, c, input];
 }
 
 module.exports = { getCalldata };
