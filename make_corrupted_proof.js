f3765d6477e49c02883f4f08f2ac8d2c02ffea8b
Mobin Saeidi <saeidimobin7@gmail.com>
2025-08-15 13:53:05 +0330

    update12

--- make_corrupted_proof.js
+++ make_corrupted_proof.js
@@ -1,32 +1,32 @@
 const fs = require("fs");
 const path = require("path");
 
-// مسیر پروف اصلی و خراب
+
 const validPath = path.join(__dirname, "circuits", "build", "proof_valid.json");
 const corruptedPath = path.join(__dirname, "circuits", "build", "proof_corrupted.json");
 
-// خواندن JSON
+
 let proofData = JSON.parse(fs.readFileSync(validPath));
 
-// گرفتن ریفرنس به محل pi_a
+
 let proofObj;
 if (proofData.proof && proofData.proof.pi_a) {
-  // ساختار دارای آبجکت proof
+
   proofObj = proofData.proof;
 } else if (proofData.pi_a) {
-  // ساختار ساده
+
   proofObj = proofData;
 } else {
-  throw new Error("ساختار proof_valid.json ناشناخته است - pi_a پیدا نشد!");
+  throw new Error("e1");
 }
 
-// خراب‌کردن اولین المان pi_a
+
 const original = proofObj.pi_a[0];
 proofObj.pi_a[0] = (BigInt(original) + 987654321n).toString();
 
-// ذخیره پروف خراب
+
 fs.writeFileSync(corruptedPath, JSON.stringify(proofData, null, 2));
 
-console.log("✅ پروف خراب ساخته شد:", corruptedPath);
-console.log("ℹ️ مقدار اصلی:", original);
-console.log("ℹ️ مقدار جدید:", proofObj.pi_a[0]);
+console.log("e2", corruptedPath);
+console.log("e3", original);
+console.log("e4", proofObj.pi_a[0]);
