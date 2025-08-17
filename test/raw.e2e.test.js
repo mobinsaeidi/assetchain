b1942c0d9f428bbc8c802d72d4c37efbe94c2ba9
Mobin Saeidi <saeidimobin7@gmail.com>
2025-08-17 18:37:56 +0330

    delet3

--- test/raw.e2e.test.js
+++ test/raw.e2e.test.js
@@ -1,5 +1,5 @@
-const { expect } = require("chai");
-const hardhat = require("hardhat");
+import { expect } from "chai";
+import hardhat from "hardhat";
 const { ethers } = hardhat;
 
 describe("MyContract", function () {
